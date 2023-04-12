2cdb8a2dd93d76ce084d93fab982aa511a45d6c3
Claudiu Macovei <makowey@gmail.com>
2023-04-12 19:33:20 +0300

    rm bg

--- src/routes/(front)/+page.svelte
+++ src/routes/(front)/+page.svelte
@@ -4,8 +4,7 @@
 
 <slot>
     <div class="center" in:fade={{ duration: 500, delay:600 }} out:fade={{ duration: 500, delay:100 }}>
-        <div class="bg-yellow-300">
-            <img src="https://www.webalive.com.au/wp-content/uploads/2019/09/ecommerce-website-features-infographic.jpg" class="h-full w-full" alt="E-Commerce best practices">
-        </div>
+        <img src="https://www.webalive.com.au/wp-content/uploads/2019/09/ecommerce-website-features-infographic.jpg"
+             class="h-full w-full" alt="E-Commerce best practices">
     </div>
 </slot>
