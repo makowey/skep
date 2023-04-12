29081005afdec362ffda1a4e4d40ca6492d4f8b3
Claudiu Macovei <makowey@gmail.com>
2023-04-12 12:13:40 +0300

    Skep logo :)

--- src/routes/(front)/+page.svelte
+++ src/routes/(front)/+page.svelte
@@ -1,1 +1,10 @@
-<slot/>
+<script>
+    import SkepLogo from "../../components/SkepLogo.svelte";
+    import {fade} from "svelte/transition";
+</script>
+
+<slot>
+    <div class="center" in:fade={{ duration: 500, delay:600 }} out:fade={{ duration: 500, delay:100 }}>
+        <SkepLogo/>
+    </div>
+</slot>
