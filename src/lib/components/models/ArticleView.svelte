72df87b22153b115e76c018d1e21cb6bb936938d
Claudiu Macovei <makowey@gmail.com>
2023-04-14 15:32:36 +0300

    resizable

--- src/lib/components/models/ArticleView.svelte
+++ src/lib/components/models/ArticleView.svelte
@@ -20,7 +20,7 @@
 
 <div class="container mx-auto my-8 flex flex-col lg:flex-row">
     <!-- Product Image Panel -->
-    <div class="w-full lg:w-1/4">
+    <div class="w-1/4 h-auto">
         <Carousel {images} />
     </div>
 
