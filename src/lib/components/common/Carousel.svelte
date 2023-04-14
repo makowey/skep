21c4ebcbd405456f3aa33f47502eb55c525ca295
Claudiu Macovei <makowey@gmail.com>
2023-04-14 15:27:13 +0300

    add new carousel

--- src/lib/components/common/Carousel.svelte
+++ src/lib/components/common/Carousel.svelte
@@ -1,15 +1,19 @@
 <script>
+    import Carousel from 'svelte-carousel'
+
     export let images = [];
 </script>
 
-<div class="carousel w-full h-auto rounded-sm">
+<Carousel
+        autoplayDuration={0}
+        duration={5000}
+        autoplay
+        timingFunction="linear"
+        dots={true}
+        arrows={true}
+        swiping={false}
+>
     {#each images as image}
-        <div id="slide{image.id}" class="carousel-item relative w-full">
-            <img src="{image.url}" class="w-full mask mask-squircle"/>
-            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide{image.id === 1 ? images.length : image.id - 1}" class="btn btn-circle">❮</a>
-                <a href="#slide{image.id === images.length ? 1 : image.id + 1}" class="btn btn-circle">❯</a>
-            </div>
-        </div>
+        <img src={image.url} alt="{image.id}">
     {/each}
-</div>
+</Carousel>
