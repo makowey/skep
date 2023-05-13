13a261641f0788fc0d1c05c8a92a859060c16b7a
Claudiu Macovei <makowey@gmail.com>
2023-05-13 10:53:14 +0300

    searchable songs

--- src/lib/components/CEFAudioPlayer.svelte
+++ src/lib/components/CEFAudioPlayer.svelte
@@ -3,6 +3,24 @@
     import 'svelte-aplayer/dist/svelte/style.css'
     import playlist from "$lib/playlist.js";
 
+    let selection = '';
+    let songs;
+    let message;
+
+    $: if(selection) {
+        songs = playlist.filter(song => song?.name?.toLowerCase().indexOf(selection.toLowerCase()) > -1 ||
+            song?.artist?.toLowerCase().indexOf(selection.toLowerCase()) > -1);
+        if(songs.length === 0) {
+            songs = [...playlist];
+        }
+    } else {
+        songs = [...playlist];
+    }
+
+    $: message = selection;
 </script>
 
-<Aplayer audio={playlist} volume={0.4} autoplay={true}/>
+
+<input type="text" bind:value={selection} class="m-2 w-96 p-1" placeholder="search for a song..."/>
+<Aplayer audio={songs} volume={0.4} autoplay={true}/>
+<p class="text-xs hidden ">{message}</p>
