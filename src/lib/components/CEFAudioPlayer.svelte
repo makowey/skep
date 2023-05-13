9277dcc6497e0747566a7bdc65b8de2bbd8c38f8
Claudiu Macovei <makowey@gmail.com>
2023-05-13 18:55:30 +0300

    new mp3player version

--- src/lib/components/CEFAudioPlayer.svelte
+++ src/lib/components/CEFAudioPlayer.svelte
@@ -1,6 +1,9 @@
 <script>
-    import Aplayer from 'svelte-aplayer/dist/svelte/svelte-aplayer.es.js'
-    import 'svelte-aplayer/dist/svelte/style.css'
+    // import Aplayer from 'svelte-aplayer/dist/svelte/svelte-aplayer.es.js'
+    // import 'svelte-aplayer/dist/svelte/style.css'
+
+    import MusicPlayer from '../components/common/MusicPlayer.svelte'
+
     import playlist from "$lib/playlist.js";
 
     let selection = '';
@@ -22,5 +25,6 @@
 
 
 <input type="text" bind:value={selection} class="m-2 w-96 p-1" placeholder="search for a song..."/>
-<Aplayer audio={songs} volume={0.4} autoplay={true}/>
+<!--<Aplayer audio={songs} volume={0.4} autoplay={true}/>-->
+<MusicPlayer />
 <p class="text-xs hidden ">{message}</p>
---
+++ static/resources/js/main.js
@@ -0,0 +1,59 @@
+(function(){
+
+  const t = {
+    playlist: [
+      {
+        trackName: 'Din cerul plin de slava',
+        trackArtist: 'AMEC - Alegerea lui Iuda',
+        thumb: 'https://edituraamec.ro/bookstore/img/p/2/9/8/8/2988-large_default.jpg',
+        file: `https://edituraamec.ro/uploads/cef_folder/worker/Sound/Alegerea lui Iuda/mp3/Din cerul plin de slava (voce).mp3`,
+        trackAlbum: "Single",
+      },
+      {
+        trackName: 'O Carte am in gand',
+        trackArtist: 'AMEC - Biserica pe intelesul prescolarilor',
+        thumb: 'https://edituraamec.ro/bookstore/img/p/3/7/7/8/3778-large_default.jpg',
+        file: `https://edituraamec.ro/uploads/cef_folder/worker/Sound/Biserica pe intelesul prescolarilor/mp3/O Carte am in gand (voce).mp3`
+      },
+      {
+        trackName: 'Sunt copil Isuse',
+        trackArtist: 'AMEC - Biserica pe intelesul prescolarilor',
+        thumb: 'https://edituraamec.ro/bookstore/img/p/3/7/7/8/3778-large_default.jpg',
+        file: `https://edituraamec.ro/uploads/cef_folder/worker/Sound/Biserica pe intelesul prescolarilor/mp3/Sunt copil Isuse (voce).mp3`
+      },
+      {
+        trackName: 'Suntem o familie unita',
+        trackArtist: 'AMEC - Biserica pe intelesul prescolarilor',
+        thumb: 'https://edituraamec.ro/bookstore/img/p/3/7/7/9/3779-large_default.jpg',
+        file: `https://edituraamec.ro/uploads/cef_folder/worker/Sound/Biserica pe intelesul prescolarilor/mp3/Suntem o familie unita (voce).mp3`
+      },
+      {
+        trackName: 'Al meu Domn e Sfant',
+        trackArtist: 'AMEC - Cartea fara Cuvinte',
+        thumb: 'https://edituraamec.ro/bookstore/img/p/2/4/0/3/2403-medium_default.jpg',
+        file: `https://edituraamec.ro/uploads/cef_folder/worker/Sound/Cartea fara Cuvinte/mp3/Al meu Domn e Sfant (voce).mp3`
+      },
+      {
+        trackName: 'Cinci culori am intr-o carte',
+        trackArtist: 'AMEC - Cartea fara Cuvinte',
+        thumb: 'https://edituraamec.ro/bookstore/img/p/2/4/0/3/2403-medium_default.jpg',
+        file: `https://edituraamec.ro/uploads/cef_folder/worker/Sound/Cartea fara Cuvinte/mp3/Cinci culori am intr-o carte (voce).mp3`
+      },
+      {
+        trackName: 'Da, inima mi-era neagra',
+        trackArtist: 'AMEC - Cartea fara Cuvinte',
+        thumb: 'https://edituraamec.ro/bookstore/img/p/2/4/0/3/2403-medium_default.jpg',
+        file: `https://edituraamec.ro/uploads/cef_folder/worker/Sound/Cartea fara Cuvinte/mp3/Da, inima mi-era neagra (voce).mp3`
+      },
+      {
+        trackName: 'Da-I povara ta lui Isus',
+        trackArtist: 'AMEC - Cartea fara Cuvinte',
+        thumb: 'https://edituraamec.ro/bookstore/img/p/2/4/0/3/2403-medium_default.jpg',
+        file: `https://edituraamec.ro/uploads/cef_folder/worker/Sound/Cartea fara Cuvinte/mp3/Da-I povara ta lui Isus (voce).mp3`
+      }
+    ]
+  };
+
+  $(".jAudio").jAudio(t);
+
+})();
